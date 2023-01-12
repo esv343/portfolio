b62133effc4a480467d6bdc4bcccf45fa3ab6a00
Elias Velazquez <evelazquez2604@gmail.com>
2023-01-12 00:02:22 -0300

    Corrige funcionalidad menu hamburguesa para resolucion 768

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,9 +1,17 @@
 const hamburger = document.getElementById("hamburger")
 const secciones = document.getElementById("secciones")
-
+const main = document.getElementById("main")
 
 hamburger.addEventListener("click", ()=>{
     hamburger.classList.toggle("active")
     secciones.classList.toggle("mostrar")
     secciones.classList.toggle("slide-right")
+
 })
+
+main.addEventListener("click", ()=>{
+    hamburger.classList.toggle("active")
+    secciones.classList.remove("mostrar")
+    secciones.classList.remove("slide-right")
+
+} )
